a96d4dca174ac720d7496090f49b7fb50e764ce2
Leo <stbirb@yandex.ru>
2019-05-09 15:55:26 +0300

    Initial commit

---
+++ src/com/alex/Main.java
@@ -0,0 +1,24 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int pounds = 15;
+
+        double kg = 0.45359237*pounds;
+        System.out.println(kg);
+
+
+
+
+
+
+
+
+
+
+
+	// write your code here
+    }
+}
